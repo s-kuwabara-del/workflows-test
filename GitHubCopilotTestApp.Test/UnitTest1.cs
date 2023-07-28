f0b985f20bce756dbef5e62d441ac1a60c21dd1f
Shugo Kuwabara <140689499+s-kuwabara-del@users.noreply.github.com>
2023-07-28 15:05:46 +0900

    [Add] Unit test.

---
+++ GitHubCopilotTestApp.Test/UnitTest1.cs
@@ -0,0 +1,59 @@
+using GitHubCopilotTestApp;
+
+namespace GitHubCopilotTestApp.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestFixture]
+        public class ProgramTest
+        {
+            Program target = new Program();
+
+
+            [TestCase(2)]
+            [TestCase(3)]
+            [TestCase(5)]
+            [TestCase(7)]
+            [TestCase(997)]
+            [TestCase(2153)]
+            [TestCase(5023)]
+            [TestCase(9999991)]
+            public void IsPrimeTest(int n)
+            {
+                var type = target.GetType();
+                var methodInfo = type.GetMethod("IsPrime", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+                if (methodInfo != null )
+                {
+                    var result = (bool)methodInfo.Invoke(target, new object[] { n });
+                    Assert.IsTrue(result);
+                }
+            }
+
+            [TestCase(-1)]
+            [TestCase(0)]
+            [TestCase(1)]
+            [TestCase(4)]
+            [TestCase(6)]
+            [TestCase(998)]
+            [TestCase(2152)]
+            [TestCase(5024)]
+            [TestCase(9999999)]
+            public void IsNotPrimeTest(int n)
+            {
+                var type = target.GetType();
+                var methodInfo = type.GetMethod("IsPrime", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+                if (methodInfo != null)
+                {
+                    var result = (bool)methodInfo.Invoke(target, new object[] { n });
+                    Assert.IsFalse(result);
+                }
+            }
+
+        }
+    }
+}
--- GitHubCopilotTestApp/Program.cs
+++ GitHubCopilotTestApp/Program.cs
@@ -2,59 +2,70 @@
 using System.Diagnostics;
 using System.Threading.Tasks;
 
-// 同期用オブジェクト
-object lockObject = new object();
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("GitHubCopilotTestApp.Test")]
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-Console.WriteLine("入力した数までの素数を表示します。");
-Console.WriteLine("数値を入力してください。");
-Console.Write("> ");
-
-var input = Console.ReadLine();
-if (string.IsNullOrEmpty(input))
+namespace GitHubCopilotTestApp
 {
-    Console.WriteLine("数値を入力してください。");
-    return;
-}
-var number = int.Parse(input);
-Console.WriteLine("1から" + number + "までの素数を表示します。");
-
-var primes = new HashSet<int>(number);
-var sw = new Stopwatch();
-var sb = new StringBuilder();
-
-sw.Start();
-
-Parallel.For(1, number, i =>
-{
-    if (IsPrime(i))
+    internal class Program
     {
-        lock (lockObject)
+        private static void Main(string[] args)
         {
-            primes.Add(i);
+            // 同期用オブジェクト
+            object lockObject = new object();
+
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("入力した数までの素数を表示します。");
+            Console.WriteLine("数値を入力してください。");
+            Console.Write("> ");
+
+            var input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("数値を入力してください。");
+                return;
+            }
+            var number = int.Parse(input);
+            Console.WriteLine("1から" + number + "までの素数を表示します。");
+
+            var primes = new HashSet<int>(number);
+            var sw = new Stopwatch();
+            var sb = new StringBuilder();
+
+            sw.Start();
+
+            Parallel.For(1, number, i =>
+            {
+                if (IsPrime(i))
+                {
+                    lock (lockObject)
+                    {
+                        primes.Add(i);
+                    }
+                }
+            });
+
+            sw.Stop();
+            Console.WriteLine("処理時間: " + sw.ElapsedMilliseconds + "ms");
+            Console.WriteLine(string.Join(" ", new SortedSet<int>(primes)));
+        }
+        private static bool IsPrime(int number)
+        {
+            // number が素数かどうかを判定する
+            if (number < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
-});
-
-sw.Stop();
-Console.WriteLine("処理時間: " + sw.ElapsedMilliseconds + "ms");
-Console.WriteLine(string.Join(" ", new SortedSet<int>(primes)));
-
-static bool IsPrime(int number)
-{
-    // number が素数かどうかを判定する
-    if (number < 2)
-    {
-        return false;
-    }
-    for (int i = 2; i * i <= number; i++)
-        {
-            if (number % i == 0)
-        {
-            return false;
-        }
-    }
-    return true;
 }
